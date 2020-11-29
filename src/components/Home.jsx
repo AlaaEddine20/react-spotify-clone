8b2c9acd0211ece8beca91a361c6ab6236542840
Alaa Eddine <alaneddin91@gmail.com>
2020-11-29 14:14:12 +0100

    trying to fetch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,36 +3,49 @@ import React from "react";
 import Sidebar from "./Sidebar";
 // import Navbar from "./Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Col, Image } from "react-bootstrap";
 
 class Home extends React.Component {
   state = {
-    linkinPark: [],
+    albums: [],
+    loading: true,
   };
 
-  componentDidMount() {
-    this.fetchSongs();
+  async componentDidMount() {
+    const getAlbum = await fetch(
+      "https://deezerdevs-deezer.p.rapidapi.com/search?q=eminem",
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-key":
+            "7058b459femsh8bbc3e5e09ff45bp16ae10jsnaa8151340a4c",
+          "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+        },
+      }
+    );
+    const data = await getAlbum.json();
+    this.setState({ albums: data, loading: false });
+    console.log(data);
   }
-  fetchSongs = () => {
-    fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=linkin%20park", {
-      method: "GET",
-      headers: {
-        "x-rapidapi-key": "7058b459femsh8bbc3e5e09ff45bp16ae10jsnaa8151340a4c",
-        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-      },
-    })
-      .then((response) => response.json())
-      .then(
-        (resp) => this.setState({ linkinPark: resp.data.title }),
-        () => console.log(this.state.linkinPark)
-      )
-      .catch((err) => {
-        console.error(err);
-      });
-  };
+
   render() {
+    const { albums, loading } = this.props;
     return (
       <div className="Home">
         {/* <Navbar /> */}
+        <div className="main-page">
+          {loading ? (
+            <h4>{loading}</h4>
+          ) : (
+            <>
+              {albums.map((album) => (
+                <Col>
+                  <Image src={album.album.cover} />
+                </Col>
+              ))}
+            </>
+          )}
+        </div>
         <Sidebar />
       </div>
     );
